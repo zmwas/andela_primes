095830d5588d85c0019a0a30f82d311eec53a606
Arnold Okoth <arnold.okoth@andela.com>
2017-06-12 17:53:07 +0300

    added test for list

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -6,3 +6,8 @@ class PrimeNumbersTestCase(unittest.TestCase):
     def test_input_is_integer(self):
         with self.assertRaises(TypeError):
              generate_prime_numbers('string')
+    def test_ouput_is_list(self):
+        result = generate_prime_numbers(10)
+        self.assertTrue(type(result) == list,msg="output is not a list")
+
+    
